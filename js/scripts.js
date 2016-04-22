344e97bd1dcf6aab2c43a20a65a807b11f252b34
Willyb15 <william.steen.bryant@gmail.com>
2016-04-22 11:23:37 -0400

    Added contact Section
    Added Contact Section

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 		
-	setTimeout(function(){
-		$('#loader').hide();
-		$(window).scrollTop(0);
-	}, 3000);
+	// setTimeout(function(){
+	// 	$('#loader').hide();
+	// 	$(window).scrollTop(0);
+	// }, 3000);
 
-	var intFrameHeight = window.innerHeight;
+	// var intFrameHeight = window.innerHeight;
 
