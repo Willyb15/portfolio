9409794ee78ad152f327d4332cd1f12b6e376867
Willyb15 <william.steen.bryant@gmail.com>
2016-04-25 08:51:23 -0400

    Got home page working.
    Got home page working. Updated About section

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,16 @@ setTimeout(function() {
 }, 4000);
 setTimeout(function() {
     $('.webdev').css("visibility", "visible").hide().fadeIn(6000);
-}, 4000);
+}, 4500);
 setTimeout(function() {
     $(".design").css("visibility", "visible").hide().fadeIn(6000);
-}, 6000);
-setTimeout(function() {
-    $(".vertical-divider").css("visibility", "visible").hide().fadeIn(5000);
-}, 6000);
-setTimeout(function() {
-    $(".code").css("visibility", "visible").hide().fadeIn(6000);
 }, 7000);
 setTimeout(function() {
-    $(".build").css("visibility", "visible").hide().fadeIn(6000);
+    $(".vertical-divider").css("visibility", "visible").hide().fadeIn(5000);
+}, 7000);
+setTimeout(function() {
+    $(".code").css("visibility", "visible").hide().fadeIn(6000);
 }, 8000);
+setTimeout(function() {
+    $(".build").css("visibility", "visible").hide().fadeIn(6000);
+}, 9000);
