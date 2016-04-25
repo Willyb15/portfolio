079a76fd94190cc3c24b7c4976acb96373559ff4
Willyb15 <william.steen.bryant@gmail.com>
2016-04-24 20:48:55 -0400

    Updated Home Screen
    Updated Home fade in options. Added some images

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
-setTimeout(function(){
-	$('#loader').hide();
-	$(window).scrollTop(0);
+setTimeout(function() {
+    $('#loader').hide();
+    $(window).scrollTop(0);
 }, 3000);
 
-	var intFrameHeight = window.innerHeight;
-	// $("#overlay").hide();
-	setTimeout(function(){
-		$("#overlay").fadeIn(5000)
-	},4000);
-
+var intFrameHeight = window.innerHeight;
+// $("#overlay").hide();
+setTimeout(function() {
+    $('.willb').css("visibility", "visible").hide().fadeIn(6000);
+}, 4000);
+setTimeout(function() {
+    $('.webdev').css("visibility", "visible").hide().fadeIn(6000);
+}, 4000);
+setTimeout(function() {
+    $(".design").css("visibility", "visible").hide().fadeIn(6000);
+}, 6000);
+setTimeout(function() {
+    $(".vertical-divider").css("visibility", "visible").hide().fadeIn(5000);
+}, 6000);
+setTimeout(function() {
+    $(".code").css("visibility", "visible").hide().fadeIn(6000);
+}, 7000);
+setTimeout(function() {
+    $(".build").css("visibility", "visible").hide().fadeIn(6000);
+}, 8000);
