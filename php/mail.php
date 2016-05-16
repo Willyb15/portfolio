21a963a2cd250f15064aaa66bf791d8ac82fab02
Willyb15 <william.steen.bryant@gmail.com>
2016-05-16 19:13:00 -0400

    Updated descriptions and php message

--- php/mail.php
+++ php/mail.php
@@ -14,40 +14,4 @@
        header("Location: http://willybman.com/php/thankyou.html");
        }
 
-// require_once "vendor/autoload.php";
-
-// //PHPMailer Object
-// $mail = new PHPMailer;
-
-// //From email address and name
-// $mail->From = "willybman@willybman.com";
-// $mail->FromName = "Will";
-
-// //To address and name
-// $mail->addAddress("william.steen.bryant@gmail.com", "Will Bryant");
-// $mail->addAddress("william.steen.bryant@gmail.com"); //Recipient name is optional
-
-// //Address to which recipient will reply
-// $mail->addReplyTo("reply@willybman.com.com", "Reply");
-
-// //CC and BCC
-// // $mail->addCC("cc@example.com");
-// // $mail->addBCC("bcc@example.com");
-
-// //Send HTML or Plain Text email
-// $mail->isHTML(true);
-
-// $mail->Subject = "From Website";
-// $mail->Body = "<i>Mail body in HTML</i>";
-// $mail->AltBody = "This is the plain text version of the email content";
-
-// if(!$mail->send()) 
-// {
-//     echo "Mailer Error: " . $mail->ErrorInfo;
-// } 
-// else 
-// {
-//     echo "Message has been sent successfully";
-// }
-
 ?>
